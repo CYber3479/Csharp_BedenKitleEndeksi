0f3878d0198f603b4ac58db73bac708024a7ea9c
CYber3479 <celalyilmazfb@gmail.com>
2022-10-02 11:06:33 +0300

    Beden kitle endeksi
    C# ile beden kitle endeksi nasıl öğrenilir?

---
+++ BedenKitleEndeksi/BedenKitleEndeksi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BedenKitleEndeksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            beden kitle endeksi = kilo / boy*boy
+            beden kitle endeksi 18'e eşit veya düşükse zayıf
+            18 ile 25 arasındaysa normal
+            25'ten büyükse obez yazdıran program parçasını kodlayınız.
+            */
+           
+            Console.WriteLine("Kilonuzu giriniz.");
+            int kilo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Boyunuzu giriniz.");
+            double boy = Convert.ToDouble(Console.ReadLine());
+
+            double bke = kilo / (boy * boy);
+        
+            if (bke<18)
+            {
+                Console.WriteLine("Zayıf");
+                Console.ReadLine();
+            }
+            else if (18<bke&&bke<25)
+            {
+                Console.WriteLine("Normal");
+                Console.ReadLine();
+            }
+            else if (bke>25)
+            {
+                Console.WriteLine("Obez");
+                Console.ReadLine();
+            }
+        }           
+    }
+}
